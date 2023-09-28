f44a84922f36eb62ffcb5514cb0fdbb8b10bfba4
nataliiazab <113237479+nataliiazab@users.noreply.github.com>
2023-09-28 19:34:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('Javascript is running')
+console.log("Javascript is running");
+
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContex("2d");
+
+const WIDTH = canvas.width;
+const HEIGHT = canvas.height;
+
+//DRAW A RECTANGLE
+
+ctx.fillStyle = "yellow";
+ctx.fillReact(WIDTH / 2 - 30, HEIGHT - 30, 60, 30); //left,top,width,height
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === " ") {
+    const bullet = new Bullet(x, y);
+  }
+});
